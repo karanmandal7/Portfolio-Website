4324bd5767d9ff29d4b2b61fba5343e94eefda6f
karan1171 <karanm98033@gmail.com>
2025-06-19 04:06:15 +0530

    Fixed projects error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
-import Projects from './pages/Projects'
+import Projects from './pages/projects'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
 
